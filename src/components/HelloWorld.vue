7f70c1368e6950fd18bc08c1b7dbd9e5368abc2e
Ericka Diaz <erickaldiaz@gmail.com>
2021-07-06 22:39:41 -0300

    practices

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,58 +1,55 @@
 <template>
   <div class="hello">
-    <h1>{{ msg }}</h1>
-    <p>
-      For a guide and recipes on how to configure / customize this project,<br>
-      check out the
-      <a href="https://cli.vuejs.org" target="_blank" rel="noopener">vue-cli documentation</a>.
-    </p>
-    <h3>Installed CLI Plugins</h3>
-    <ul>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-babel" target="_blank" rel="noopener">babel</a></li>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-eslint" target="_blank" rel="noopener">eslint</a></li>
-    </ul>
-    <h3>Essential Links</h3>
-    <ul>
-      <li><a href="https://vuejs.org" target="_blank" rel="noopener">Core Docs</a></li>
-      <li><a href="https://forum.vuejs.org" target="_blank" rel="noopener">Forum</a></li>
-      <li><a href="https://chat.vuejs.org" target="_blank" rel="noopener">Community Chat</a></li>
-      <li><a href="https://twitter.com/vuejs" target="_blank" rel="noopener">Twitter</a></li>
-      <li><a href="https://news.vuejs.org" target="_blank" rel="noopener">News</a></li>
-    </ul>
-    <h3>Ecosystem</h3>
-    <ul>
-      <li><a href="https://router.vuejs.org" target="_blank" rel="noopener">vue-router</a></li>
-      <li><a href="https://vuex.vuejs.org" target="_blank" rel="noopener">vuex</a></li>
-      <li><a href="https://github.com/vuejs/vue-devtools#vue-devtools" target="_blank" rel="noopener">vue-devtools</a></li>
-      <li><a href="https://vue-loader.vuejs.org" target="_blank" rel="noopener">vue-loader</a></li>
-      <li><a href="https://github.com/vuejs/awesome-vue" target="_blank" rel="noopener">awesome-vue</a></li>
-    </ul>
+  <h1>el mensaje es: {{ msg }}</h1>
+  <h1 v-text="msg">hola</h1>
+
+<!-- a partir de aca V-HTML -->
+  <p>esto es una etiqueta p</p>
+  <p v-html="htmlEjemplo"></p>
+
+  <!-- A PARTIR DE ACA V SHOW -->
+  <h2 v-show="acceso">Tenemos accesos aa esto</h2>
+
+  <!-- A PARTIR DE ACA v-IF -->
+  <h3 v-if="accesoComun"> Tenemos acceso</h3>
+  <h3 v-else-if="accesoAdministrador"> Tenemos acceso de administrador</h3>
+  <h3 v-else>No tienes accesos para ver esto</h3>
+
+  <!-- A PARTIR DE ACA v-for -->
+  <div v-for="(alumno, index) in alumnos" :key="index">
+    <p>{{alumno.nombre}}</p>
+    <p>{{alumno.edad}}</p>
+  </div>
+  
   </div>
 </template>
 
 <script>
 export default {
   name: 'HelloWorld',
-  props: {
-    msg: String
+  data: () => ({
+    msg:'hola, bienvenidos',
+    htmlEjemplo: '<p>esto es una etiqueta p</p>',
+    accesoComun: false,
+    accesoAdministrador: false,
+    alumnos: [   
+    { 
+      nombre:"Matias",
+      edad: 18
+    },
+    { 
+      nombre:"Lara",
+      edad: 20
+    },
+    { 
+      nombre:"Juan",
+      edad: 38
+    },
+    { 
+      nombre:"Daina",
+      edad: 25
+    }
+    ]
+    })
   }
-}
 </script>
-
-<!-- Add "scoped" attribute to limit CSS to this component only -->
-<style scoped>
-h3 {
-  margin: 40px 0 0;
-}
-ul {
-  list-style-type: none;
-  padding: 0;
-}
-li {
-  display: inline-block;
-  margin: 0 10px;
-}
-a {
-  color: #42b983;
-}
-</style>
