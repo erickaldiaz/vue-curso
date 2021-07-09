1bbd6cbf953534b6633d62b1d80cf3728e110a4a
Ericka Diaz <erickaldiaz@gmail.com>
2021-07-08 22:20:53 -0300

    create a new component

---
+++ src/components/DirectivasExample.vue
@@ -0,0 +1,29 @@
+<template>
+    <el-row type="flex"  justify="center">
+        <el-col :span="6"> 
+            <el-button @click="changeTheVisibilityOfTheMessage" type="primary">
+                <p v-show="!show"></p>
+            </el-button>
+        </el-col>
+        <el-col :span="6"> 
+            <h1 v-show="show">
+                El mensaje secreto es: ....
+            </h1> 
+        </el-col>
+        <el-col :span="6"></el-col>
+    </el-row>
+</template>
+
+<script>
+export default {
+        data: () => ({
+            show: false
+        }),
+        methods: {
+            changeTheVisibilityOfTheMessage(){
+                this.show = !this.show;
+            }
+        }
+}
+</script>
+
