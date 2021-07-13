c3287961ebf455b11cbb7205ce628d8414a93ac1
Ericka Diaz <erickaldiaz@gmail.com>
2021-07-13 20:16:59 -0300

    add DirectivasBindAndModel.vue v-model

--- src/App.vue
+++ src/App.vue
@@ -2,19 +2,22 @@
   <div id="app">
     <HelloWorld v-show="false"/>
     <DirectivasExample/>
+    <DirectivasBindAndModel/>
   </div>
 </template>
 
 <script>
 import HelloWorld from './components/HelloWorld.vue'
 import DirectivasExample from './components/DirectivasExample.vue'
+import DirectivasBindAndModel from './components/DirectivasBindAndModel.vue'
 
 
 export default {
   name: 'App',
   components: {
     HelloWorld,
-    DirectivasExample
+    DirectivasExample,
+    DirectivasBindAndModel
   }
 }
 </script>
---
+++ src/components/DirectivasBindAndModel.vue
@@ -0,0 +1,24 @@
+<template>
+    <el-row type="flex" justify="center">
+        <el-col :span="6">
+            <!-- <img :src="url" : [atributoMisterioso]="atributoMisteriosoValue"> -->
+            <h1>{{edad}}</h1>
+            <el-input v-model.lazy="edad">
+            </el-input>
+        </el-col>  
+    </el-row>
+</template>
+
+<script>
+export default {
+    data: ()=> ({
+        url:"https://pbs.twimg.com/media/EWPJiRLU8AIbuyb.jpg",
+        atributoMisterioso: 'alt',
+        atributoMisteriosoValue: 'Foto de Bob Esponja',
+        edad:''
+    }),
+    methods: {
+
+    }
+}
+</script>
